1c05925fbeaf8736ec96dd577e5b7519892457a3
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-13 13:52:16 -0600

    Project screen layout

--- client/src/screens/projects/Project1.jsx
+++ client/src/screens/projects/Project1.jsx
@@ -2,8 +2,27 @@ import './Projects.css'
 
 export default function Project1() {
   return (
-    <div>
-
-    </div>
+    <section id='project-one' className='project-one-page'>
+      <div className='project-container'>
+        <img src=''
+          className='project-screenshot'
+          alt='landing page for Hogwarts Hopefuls app' />
+        <div className='project-description-header'>
+          | DESCRIPTION |
+        </div>
+        <div className='project-description-text'>
+          For this project
+        </div>
+        <div className='project-resources-header'>
+          | LANGUAGES, LIBRARIES, FRAMEWORKS |
+        </div>
+        <div className='project-resources-text'>
+          React, Ruby...
+        </div>
+      </div>
+      <div className='project-scroll-text'>
+        KEEP CALM AND SCROLL ON
+      </div>
+    </section>
   )
 }
