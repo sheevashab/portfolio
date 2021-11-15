3f23144784579dbd26df88cc8fe78b3edf372428
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-11-14 19:07:26 -0600

    resume link added, site fully styled for mobile and deployed

--- client/src/screens/contact/Contact.jsx
+++ client/src/screens/contact/Contact.jsx
@@ -1,4 +1,7 @@
 import './Contact.css';
+import { NavHashLink } from 'react-router-hash-link';
+import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
+import IconButton from '@mui/material/IconButton';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
@@ -11,15 +14,52 @@ export default function Contact() {
         CONTACT
       </div>
       <div className='contact-links'>
-        <div><LinkedInIcon fontSize="large" /></div>
-        <a href='https://www.linkedin.com/in/sheeva-shabahang-2255a562/' target='_blank' rel="noreferrer">linkedin</a>
-        <div><GitHubIcon fontSize="large" /></div>
-        <a href='https://github.com/sheevashab/' target='_blank' rel="noreferrer">github</a>
-        <div><EmailRoundedIcon fontSize="large" /></div>
-        <a href='mailto:sheeva.shabahang@gmail.com'>email</a>
-        <div><AttachFileRoundedIcon fontSize="large" /></div>
-        <div>resume</div>
+        <span className="parent-span">
+          <IconButton
+            aria-label='sheeva-shabahang-linkedin'
+            onClick={() => window.open('https://www.linkedin.com/in/sheeva-shabahang-2255a562/')}
+            sx={{ color: '#EDF2F3', }}>
+            <LinkedInIcon fontSize="large" />
+            <h5 className='contact-link'>linkedin</h5>
+          </IconButton>
+        </span>
+
+        <span className="parent-span">
+          <IconButton
+            aria-label='sheeva-shabahang-github'
+            onClick={() => window.open('https://github.com/sheevashab/')}
+            sx={{ color: '#EDF2F3', }}>
+            <GitHubIcon fontSize="large" />
+            <h5 className='contact-link'>github</h5>
+          </IconButton>
+        </span>
+
+        <span className="parent-span">
+          <IconButton
+            aria-label='sheeva-shabahang-email'
+            onClick={() => window.open('mailto:sheeva.shabahang@gmail.com')}
+            sx={{ color: '#EDF2F3', }}>
+            <EmailRoundedIcon fontSize="large" />
+            <h5 className='contact-link'>email</h5>
+          </IconButton>
+        </span>
+
+        <span className="parent-span">
+          <IconButton
+            aria-label='sheeva-shabahang-resume'
+            onClick={() => window.open('https://res.cloudinary.com/dyyjvyqtn/image/upload/v1636938339/SheevaShabahang_ResumeLink_xnw7uo.jpg')}
+            sx={{ color: '#EDF2F3', }}>
+            <AttachFileRoundedIcon fontSize="large" />
+            <h5 className='contact-link'>resume</h5>
+          </IconButton>
+        </span>
       </div>
-    </section>
+      <div className='contact-scroll-text'>
+        <NavHashLink smooth to='#landing'>
+          <KeyboardArrowUpRoundedIcon sx={{ color: '#E8CBB1' }} />
+          SCROLL HOME
+        </NavHashLink>
+      </div>
+    </section >
   )
 }
