7911dd19a43395f972b724e7c58cbf2b085d2f11
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2021-12-10 11:38:43 -0600

    dropdown menu customization

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,8 +5,26 @@ import { NavHashLink } from 'react-router-hash-link';
 import { Button, Menu, MenuItem } from '@mui/material';
 import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded';
 import HorizontalRuleSharpIcon from '@mui/icons-material/HorizontalRuleSharp';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  menu: {
+    '& .MuiPaper-root': {
+      backgroundColor: 'transparent',
+      color: '#5289B5',
+      '& .MuiMenuItem-root': {
+        fontFamily: 'Raleway, sans-serif',
+        fontSize: 16,
+        '&:hover': {
+          color: '#AFD8F2',
+        }
+      }
+    },
+  },
+})
 
 export default function Navbar() {
+  const classes = useStyles()
   const [toggle, setToggle] = useState(null)
   const open = Boolean(toggle)
 
@@ -43,29 +61,37 @@ export default function Navbar() {
               <HorizontalRuleSharpIcon />
             )}
           </Button>
+
           <Menu
+            className={classes.menu}
             id='nav-menu'
             anchorEl={toggle}
             open={open}
             onClose={handleClose}
-            transitionDuration={1000}
-            sx={{ color: '#AFD8F2' }}
+            transitionDuration={770}
           >
             <NavHashLink className='nav-link' smooth to='#about'>
-              <MenuItem key='nav-link-about' onClick={handleClose}>
+              <MenuItem
+                key='nav-link-about'
+                className={classes.menuItem}
+                onClick={handleClose}
+              >
                 About Me
               </MenuItem>
             </NavHashLink>
+
             <NavHashLink className='nav-link' smooth to='#portfolio'>
-              <MenuItem key='nav-link-projects' onClick={handleClose}>
+              <MenuItem key='nav-link-projects' className={classes.menuItem} onClick={handleClose}>
                 Portfolio
               </MenuItem>
             </NavHashLink>
+
             <NavHashLink className='nav-link' smooth to='#contact'>
-              <MenuItem key='nav-link-contact' onClick={handleClose}>
+              <MenuItem key='nav-link-contact' className={classes.menuItem} onClick={handleClose}>
                 Contact
               </MenuItem>
             </NavHashLink>
+
           </Menu>
         </nav>
       </HideOnScroll>
